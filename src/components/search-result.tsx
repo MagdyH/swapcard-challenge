b60f5b15111f9acc89bfdb33608d6574242c6d69
Magdy Hussien <32831425+MagdyH@users.noreply.github.com>
2020-09-21 22:51:29 +0200

    update

--- src/components/search-result.tsx
+++ src/components/search-result.tsx
@@ -7,34 +7,30 @@ export function SearchResult(props: any) {
     return (
         <div className={'container'}>
             {
-                props.loading && <div className="loader"><Loader
+                props.loading ? <div className="loader"><Loader
                     type="Puff"
                     color="rgb(0, 204, 136)"
                     height={100}
                     width={100}
                     timeout={30000}
 
-                /></div>
+                /></div> : (
+                        props.artists?.length > 0 ? <>
+                            <div className={'row'}><span className={'total'}>Total Count:</span> {props.totalCount}</div>
+                            <div className={'row'}>
+                                {
+                                    props.artists?.map((artist: any) => (
+                                        <div className={'col-4'}>
+                                            <ArtistCard artist={artist} />
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </> : props.error && <div className="no-result">
+                            No Result found.
+                    </div>)
             }
-            {
-                props.artists?.length > 0 && <>
-                    <div className={'row'}><span className={'total'}>Total Count:</span> {props.totalCount}</div>
-                    <div className={'row'}>
-                        {
-                            props.artists?.map((artist: any) => (
-                                <div className={'col-4'}>
-                                    <ArtistCard artist={artist} />
-                                </div>
-                            ))
-                        }
-                    </div>
-                </>
-            }
-            {
-                props.error && <div className="no-result">
-                    No Result found.
-                </div>
-            }
+
         </div>
     )
 }
